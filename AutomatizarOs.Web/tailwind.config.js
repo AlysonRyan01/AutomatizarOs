5868e08e916e4bd1e65ccb0dec0752a547825a9a
AlysonRyan01 <sdhuahdsaih@gmail.com>
2025-04-30 17:39:32 -0300

    C:\Users\TVS'PC\Documents\MeusProjetos\AutomatizarOs\AutomatizarOs.Api\meubanco.db-shm

---
+++ AutomatizarOs.Web/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./**/*.{html, js, razor}"
+  ],
+  theme: {
+    colors: {
+      "primary": "#FFFFFF",
+      "black": "#000000",
+    },
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      titillium: ['Titillium Web', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
